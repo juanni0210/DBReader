0c91eff5ec1682c280a51160df143f6f615af451
juanni0210 <janeni1020@gmail.com>
2021-03-03 15:12:13 -0500

    change TestModel import assertion to be from junit 5, remove dependency added last time from pom file

--- test/jdbc/TestModel.java
+++ test/jdbc/TestModel.java
@@ -1,9 +1,10 @@
 package jdbc;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.sql.SQLException;
 import java.util.List;
 import org.junit.jupiter.api.AfterEach;
